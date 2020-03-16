a5e8bdd55f68c64eb57a632775656162ef47fd1e
helloMikkie <mh@groenlandberlin.de>
2020-03-16 13:52:59 +0100

    mouse-over fullscreen

--- site/snippets/comp-cards.php
+++ site/snippets/comp-cards.php
@@ -35,7 +35,10 @@
     <?php elseif ($part->layout() == 'black') : ?>
         <?php foreach ($part->images() as $image) : ?>
             <article class="project-card">
-                <img src="<?= $image->url() ?>" alt="Image">
+            <div class="project-card-image">
+                <img src="<?= $image->url() ?>" alt="Image" >
+                <img src="assets/icons/ikon-fullscreen.svg" alt="ikon-fullscreen" class="ikon-fullscreen">
+            </div>
                 <h3 class="project-card-title"><?= $image->project() ?></h3>
             </article>
         <?php endforeach ?>
